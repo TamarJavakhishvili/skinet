69569cf09e2507cfc5660cfe41f3be47fc5fcd35
TamarJavakhishvili <tamuna.javakhishvili@gmail.com>
2025-05-28 18:50:30 +0400

    Angular Routing added

---
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../../core/services/shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../shared/models/product';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent implements OnInit{
+  private shopService = inject(ShopService);
+  private activatedRoute = inject(ActivatedRoute);
+  product?: Product;
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if(!id) return;
+    this.shopService.getProduct(+id).subscribe({  //cast into number adding +
+      next: product => this.product = product,
+      error: error => console.log(error)
+    })
+  }
+}
