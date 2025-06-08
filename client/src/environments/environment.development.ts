7b233dd92545934fca2df68a29df08a8e2f1d0a6
TamarJavakhishvili <tamuna.javakhishvili@gmail.com>
2025-06-08 22:53:59 +0400

    Client Checkout, address, shipping, payment functionality

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51RVcSj8HBk9uv9Qrf3saLmTwW9u17FKFTtfxlRVKRetpAerYD1jeLbalHLFjwJgbSjLYlvw7STi5TnEl5kZozCr800iV0MyZZH'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51RVcSj8HBk9uv9Qrf3saLmTwW9u17FKFTtfxlRVKRetpAerYD1jeLbalHLFjwJgbSjLYlvw7STi5TnEl5kZozCr800iV0MyZZH'
 };
