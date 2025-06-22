1183d956693ebc9b054630786b2fe188472fe694
TamarJavakhishvili <tamuna.javakhishvili@gmail.com>
2025-06-22 16:07:51 +0400

    Webhook endpoint added, StripeCli, SignalR added, Checkout success and Home pages updated

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
+
     stripePublicKey: 'pk_test_51RVcSj8HBk9uv9Qrf3saLmTwW9u17FKFTtfxlRVKRetpAerYD1jeLbalHLFjwJgbSjLYlvw7STi5TnEl5kZozCr800iV0MyZZH'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51RVcSj8HBk9uv9Qrf3saLmTwW9u17FKFTtfxlRVKRetpAerYD1jeLbalHLFjwJgbSjLYlvw7STi5TnEl5kZozCr800iV0MyZZH'
 };
